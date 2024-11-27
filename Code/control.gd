d4909439b65e2458acb7b2a1704632d7fc9f093d
john hinch <johnhnch864@gmail.com>
2024-11-27 15:00:13 +0000

    Sounds

--- Code/control.gd
+++ Code/control.gd
@@ -3,6 +3,13 @@ extends Control
 var sound_sequence=[]
 var animation_queue=[]
 
+var chords={
+	"E Chord":preload("res://Sounds/Guitar/e_major.mp3"),
+	"A Chord":preload("res://Sounds/Guitar/a_major.mp3"),
+	"D Chord":preload("res://Sounds/Guitar/d_major.mp3"),
+	"C Chord":preload("res://Sounds/Guitar/c_major.mp3"),
+	"G Chord":preload("res://Sounds/Guitar/g_major.mp3")
+}
 
 var animation_sources={
 	"guitar":preload("res://Animations/Animation_Scenes/guitar_playing.tscn"),
@@ -13,22 +20,33 @@ var animation_sources={
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$"E Chord".pressed.connect(() -> _on_button_pressed("E Chord"))
+	#$"E Chord".connect("pressed",_on_button_pressed,chords["E Chord"])
+	$"A Chord".pressed.connect(_on_button_pressed("A Chord"))
+	$"D Chord".pressed.connect(_on_button_pressed("D Chord"))
+	$"G Chord".pressed.connect(_on_button_pressed("G Chord"))
+	$"C Chord".pressed.connect(_on_button_pressed("C Chord"))
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_button_pressed() -> void:
-	if animation_queue.size()>0:
-		Global.animation_queue=animation_queue
-		get_tree().change_scene_to_file("res://Performance.tscn")
-	pass # Replace with function body.
+func _on_button_pressed(button_name:String):
+	if chords.has(button_name):
+		var sound = AudioStreamPlayer.new()
+		sound.stream=chords[button_name]
+		add_child(sound)
+		sound.play()
+		
+		#sound.connect("finsihed",sound,"queue_free")
+		GlobalSoundManager.sound_sequence.append(button_name)
+	print("Button Pressed: %s",% button_name)
 
 func _on_guitar_pressed():
 	_add_animation("Guitar")
+	
 	pass # Replace with function body.
 
 func _add_animation(animation_name):
