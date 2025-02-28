f95dd570049f6b9a7ea3ad99de673344125d5181
john hinch <johnhnch864@gmail.com>
2025-02-28 17:08:15 +0000

    VR Controls

--- Code/pause.gd
+++ Code/pause.gd
@@ -12,18 +12,16 @@ func set_paused(value:bool) ->void:
 	_is_paused = value
 	get_tree().paused = _is_paused
 	visible = _is_paused
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
 func _on_resume_pressed():
 	_is_paused = false
 	
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_left_button_pressed(name):
+	if name=="by_button":
+		_is_paused=!_is_paused
+		
+	pass # Replace with function body.
